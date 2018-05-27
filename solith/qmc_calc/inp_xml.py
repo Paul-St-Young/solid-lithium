4ad66aebcf9a93b706d2cacceb45f6c4443cb394
Paul Young <yyang173@illinois.edu>
2018-05-27 15:45:25 -0500

    pset_from_pos and psp_ham

--- solith/qmc_calc/inp_xml.py
+++ solith/qmc_calc/inp_xml.py
@@ -1,5 +1,67 @@
+import numpy as np
+from copy import deepcopy
 from lxml import etree
 from qharv.seed import xml, xml_examples
+
+
+def pset_from_pos(pos):
+  """ construct <particleset name="ion0"> from position array
+  Args:
+    pos (np.array): list of atomic positions
+  Return:
+    lxml.Element: <particleset>
+  """
+  name = 'Li'
+  charge = 1  # [He] core BFD psp
+  atomic_number = 3  # needed to check with pseudopotential
+
+  natom = len(pos)
+
+  pset = etree.Element('particleset', {'name': 'ion0'})
+  group = etree.Element('group', {
+    'name': 'Li',
+    'size': str(natom)
+  })
+  xml.set_param(group, 'charge', ' ' + str(charge) + ' ', new=True)
+  xml.set_param(group, 'atomicnumber', ' ' + str(atomic_number) + ' ', new=True)
+
+  pos_arr = etree.Element('attrib', {
+    'name': 'position',
+    'datatype': 'posArray',
+    'condition': '0'
+  })
+  pos_arr.text = xml.arr2text(pos)
+
+  group.append(pos_arr)
+  pset.append(group)
+  return pset
+
+
+def psp_ham(psp='Li.BFD.xml'):
+  """ construct <hamiltonian> using given pseudopotential
+  Args:
+    pos (np.array): list of atomic positions
+  Return:
+    lxml.Element: <particleset>
+  """
+  name = 'Li'
+  ham = xml_examples.static_coul_ae_ham()
+  ei = ham.find('.//pairpot[@name="ElecIon"]')
+  ham.remove(ei)
+  ei1 = etree.Element('pairpot', {
+    'type': 'pseudo',
+    'name':'PseudoPot',
+    'source': 'ion0',
+    'wavefunction': 'psi0',
+    'format': 'xml'
+  })
+  pseudo = etree.Element('pseudo', {
+    'elementType': name,
+    'href': psp
+  })
+  ei1.append(pseudo)
+  ham.append(ei1)
+  return ham
 
 
 def dmc_sections(tsl, dt=0.4, nwalker=576):
@@ -34,10 +96,11 @@ def li_estimators():
     'type': 'csk',
     'name': 'csk'
   })
+  bins = 256
   gr_est = etree.Element('estimator', {
     'type': 'gofr',
     'name': 'gofr',
-    'num_bin': 256
+    'num_bin': str(bins)
   })
   p_est = etree.Element('pressure', {
     'type': 'Pressure'
