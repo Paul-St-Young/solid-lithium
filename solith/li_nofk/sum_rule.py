cf3c24bc3b54e304100188990c5d7ede4d2d8351
Paul Young <yyang173@illinois.edu>
2019-08-04 16:20:21 -0500

    estimate sum-rule errors

--- solith/li_nofk/sum_rule.py
+++ solith/li_nofk/sum_rule.py
@@ -11,7 +11,7 @@ def kvol3d(rs):
   kvol = (2*np.pi)**3/(4*np.pi*rs**3/3)
   return kvol
 
-def ntsum_raw3d(kvecs, nkm, kvol):
+def ntsum_raw3d(kvecs, nkm, kvol, nke=None):
   """ Calculate momentum distribution sum rules using raw n(k) in 3D
 
   !!!! kvecs must be on a simple cubic lattice
@@ -20,6 +20,7 @@ def ntsum_raw3d(kvecs, nkm, kvol):
     kvecs (np.array): kvectors
     nkm (np.array): momentum distribution mean
     kvol (float): reciprocal space volume of a state, norm=1/kvol
+    nke (np.array, optional): momentum distribution error, default None
   Return:
     (float, float): (nsum, tsum)
   """
@@ -29,9 +30,13 @@ def ntsum_raw3d(kvecs, nkm, kvol):
   kmags = np.linalg.norm(kvecs, axis=-1)
   nsum = dk**3*nkm.sum()/kvol
   tsum = dk**3*(0.5*kmags**2*nkm).sum()/kvol
+  if nke is not None:
+    nsume = dk**3*(nke**2).sum()**0.5/kvol
+    tsume = dk**3*(0.5*kmags**2*nke**2).sum()**0.5/kvol
+    return nsum, nsume, tsum, tsume
   return nsum, tsum
 
-def ntsum_iso3d(uk, unkm, kvol):
+def ntsum_iso3d(uk, unkm, kvol, nke=None):
   """ Calculate momentum distribution sum rules using spherically-averaged
   isotropic n(k) in 3D
 
@@ -39,9 +44,14 @@ def ntsum_iso3d(uk, unkm, kvol):
     uk (np.array): unique kvector magnitudes
     unkm (np.array): momentum distribution mean
     kvol (float): reciprocal space volume of a state, norm=1/kvol
+    nke (np.array, optional): momentum distribution error, default None
   Return:
     (float, float): (nsum, tsum)
   """
   nsum = 4*np.pi*np.trapz(uk**2*unkm, uk)/kvol
   tsum = 4*np.pi*np.trapz(0.5*uk**4*unkm, uk)/kvol
+  if nke is not None:
+    nsume = 4*np.pi*np.trapz(uk**2*nke**2, uk)**0.5/kvol
+    tsume = 4*np.pi*np.trapz(0.5*uk**4*nke**2, uk)**0.5/kvol
+    return nsum, nsume, tsum, tsume
   return nsum, tsum
